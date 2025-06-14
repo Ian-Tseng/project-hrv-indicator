0bb56dbfc1f5efbc3e7a0edceeec1f8377154a15
YinYan Tseng <choisyin@gmail.com>
2025-06-14 22:28:45 +0800

    Add existing file

--- companion/index.js
+++ companion/index.js
@@ -11,11 +11,11 @@ if (!companion.permissions.granted("access_internet")) {
 }
 var state= 0;
 var heart_rate_change= [];
-const url= 'https://3f75-114-40-19-101.ngrok-free.app';
+const url= 'https://290f-114-40-46-48.ngrok-free.app ';
 var subscribe_url =url+'/subscribe';
 var unsubscribe_url= url+'/unsubscribe';
 var publish_url =url+'/publish';
-const uid= 'abc'
+const uid= 'abc';
 
 
 messaging.peerSocket.addEventListener("message", (evt) => {
@@ -36,26 +36,7 @@ messaging.peerSocket.addEventListener("message", (evt) => {
 });
 
 async function fetch_data_upload_data(evt) {
-   // var high_heart_rate= 126; //126
-   // var low_heart_rate=50; //50
-    //var gain=100;
-   // var effect_by_random= Math.random() * (1 - 0.5) + 0.5;
-    //const bio_data= (parseFloat(target_data)-low_heart_rate)/(high_heart_rate-low_heart_rate)*gain;
-
-   // heart_rate_change.splice(0, 0, target_data);
-   // if (heart_rate_change.length>3){
-   //     heart_rate_change.pop()
-   // }
-   // var dimmingSetting=  Math.round((Math.abs((heart_rate_change.at(0)-heart_rate_change.at(1)))/(high_heart_rate-low_heart_rate)+Math.abs((heart_rate_change.at(1)-heart_rate_change.at(2)))/(high_heart_rate-low_heart_rate))*100);
-
-
-   // if (dimmingSetting<0){
-   //     dimmingSetting=0;
-   // } else if (dimmingSetting>100){
-   //     dimmingSetting=100;
-   // }
-   // console.log(`Converted heart rate data to dimming: ${dimmingSetting}`)
-    // Set headers
+  
     const headers=new Headers({
         "Content-Type": "application/json",
     });
@@ -64,7 +45,7 @@ async function fetch_data_upload_data(evt) {
         'topic':evt.data["title"],
         'heart_rate_data': evt.data["data"],
         'uid':uid,
-        'for_event':'hrv_displayer_event',
+        'for_event':'hrv_indicator_event',
     }
    
     let fetch_init = {method:'POST', headers: headers, body:JSON.stringify(data), mode: "cors"}
@@ -77,10 +58,7 @@ async function fetch_data_upload_data(evt) {
 }
 
 async function fetch_data_control(target_data) {
-   // var dimmingSetting=0;
-   // if (target_data!=0){
-   //     dimmingSetting=100;
-   // }
+ 
 
     
     const headers=new Headers({
@@ -95,7 +73,7 @@ async function fetch_data_control(target_data) {
  //   }
     var data= {
         'uid':uid,
-        'topic':'hrv_displayer_event',
+        'topic':'hrv_indicator_event',
         'subscribe_data_topic': 'heart_rate_data',
     }
     let fetch_init = {method:'POST', headers: headers, body:JSON.stringify(data), mode: "cors"}
